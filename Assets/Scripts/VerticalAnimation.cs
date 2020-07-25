8dd1b81cba90dcf8a93aa663406d00ea991d3b79
somehalaldev79 <halaldev@users.noreply.github.com>
2020-07-25 19:18:50 +0500

    Beta Version 1.0
    Completed all Post Processing Effects, Levels(updated)
    Menus
    Not Added: Audio, Ad Integration, Credits, Mute Audio Button from settings

---
+++ Assets/Scripts/VerticalAnimation.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+public class VerticalAnimation : MonoBehaviour
+{
+    void Start()
+    {
+        LeanTween.moveY(gameObject,3f,1.5f).setEaseInOutCubic().setLoopPingPong();
+    }
+}
